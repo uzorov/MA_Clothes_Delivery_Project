0438a6798776e897831c9c66e49a0098cd937d8d
SashaPotemkin <alex.potjomkin@gmail.com>
2024-01-02 17:20:51 +0300

    RabbitMQ for payment/order

--- app_order/app/rabbitmq.py
+++ app_order/app/rabbitmq.py
@@ -23,14 +23,25 @@ async def process_discount(msg: IncomingMessage):
         traceback.print_exc()
         await msg.ack()
 
+async def process_paid_order(msg: IncomingMessage):
+    try:
+        data = json.loads(msg.body.decode())
+        order_id = data['order_id']
+        order_service = OrderService(OrderRepo())
+        order_service.paid_order(order_id)
+    except:
+        await msg.ack()
 
 async def consume_tasks(loop: AbstractEventLoop) -> AbstractRobustConnection:
     connection = await connect_robust(settings.amqp_url, loop=loop)
     channel = await connection.channel()
 
     task_created_queue = await channel.declare_queue('process_discount_queue', durable=True)
-
+    paid_order_queue = await channel.declare_queue('payment_queue', durable=True)
+    await paid_order_queue.consume(process_paid_order)
     await task_created_queue.consume(process_discount)
-    print('Started RabbitMQ consuming for Task Management...')
+    print('Started RabbitMQ consuming...')
 
     return connection
+
+
