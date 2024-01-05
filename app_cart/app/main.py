ac200734440ae733a7273fab4c7ce55282022377
Onestus <vkryazhevskikh@gmail.com>
2024-01-05 16:39:37 +0300

    cart rofls x1

--- app_cart/app/main.py
+++ app_cart/app/main.py
@@ -4,6 +4,6 @@ from app.endpoints.cart_router import cart_router, metrics_router
 
 app = FastAPI(title='Cart Service')
 
-app.include_router(cart_router)
+app.include_router(cart_router, prefix='/api')
 app.add_middleware(SessionMiddleware, secret_key='karaseva1234')
 app.include_router(metrics_router)
