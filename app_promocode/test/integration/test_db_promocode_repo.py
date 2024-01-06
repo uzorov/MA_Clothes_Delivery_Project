febd58b21da83d5392dc59aaaa2732d5644cb66d
SashaPotemkin <alex.potjomkin@gmail.com>
2024-01-06 19:51:53 +0300

    cicd tests rofls x23

--- app_promocode/test/integration/test_db_promocode_repo.py
+++ app_promocode/test/integration/test_db_promocode_repo.py
@@ -15,7 +15,7 @@ def first_promocode() -> Promocode:
 
 @pytest.fixture(scope='session')
 def second_promocode() -> Promocode:
-    return Promocode(id=uuid4(), code="code", discount=0.20)
+    return Promocode(id=uuid4(), code="ANOTHERCODE", discount=0.20)
 
 
 def test_add_first_promocode(first_promocode: Promocode, promocode_repo: PromocodeRepo) -> None:
@@ -25,3 +25,6 @@ def test_add_first_promocode(first_promocode: Promocode, promocode_repo: Promoco
 
 def test_add_second_promocode(second_promocode: Promocode, promocode_repo: PromocodeRepo) -> None:
     assert promocode_repo.create_promocode(second_promocode.code, second_promocode.discount) == second_promocode
+    promocodes = promocode_repo.get_promocodes()
+    assert len(promocodes) == 2
+    assert promocodes[1] == second_promocode
